0b2d46db5a50253f32a9c3cb5503960b72e4ad06
Sinevia <info@sinevia.com>
2017-11-19 07:07:53 +0000

    Update fastest.php

--- src/fastest.php
+++ src/fastest.php
@@ -1,1 +1,9 @@
-
+function fastest($options=[]){
+    $action = trim($_REQUEST['a']??'home');
+    $function = $action . '_action';
+    if (function_exists($function)) {
+       die (call_user_func($function));
+    } else {
+       header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
+    }
+}
