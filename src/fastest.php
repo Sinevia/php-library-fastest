97e7c5c80b637178717e7b6ad45a6539856c1cf6
Sinevia <info@sinevia.com>
2019-01-13 20:34:04 +0000

    Added output as string

--- src/fastest.php
+++ src/fastest.php
@@ -3,11 +3,18 @@
 namespace Sinevia;
 
 function fastest($options = []) {
-    $action = trim(str_replace(['__'],'_',str_replace(['/',' ','-'],'_',$_REQUEST['a'] ?? 'home')),'_');
+    $action = $action = trim(str_replace(['__'], '_', str_replace(['/', ' ', '-'], '_', $_REQUEST['a'] ?? 'home')), '_');
+    $outputAsString = isset($options['output_as_string']) ? $options['output_as_string'] : false;
     $function = $action . '_action';
     if (function_exists($function)) {
+        if($outputAsString){
+            return call_user_func($function);
+        }
         die(call_user_func($function));
     } else {
+        if($outputAsString){
+            return $function . ' not found';
+        }
         header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
         die($function . ' not found');
     }
